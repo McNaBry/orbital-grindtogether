a0e13fdea263e6485cd929b60b9d80e680ccb44d
McNaBry <mcnabry123@gmail.com>
2023-05-23 18:50:18 +0800

    Added a starter page to link to sign in and sign up

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-export default function Home() {
+import "./start.css"
+import Link from "next/link"
+
+export default function Start() {
   return (
     <main>
-      <div>
-        <h2>Welcome to GrindTogether!</h2>
-        <p>Check out <a href="sign-in">/sign-in</a> to see the about page</p>
+      <div id="start-container">
+        <h2 id="header">GrindTogether.</h2>
+        <h5 id="sub-header">Study. Chill. Make Friends.</h5>
+        <div id="sign-container">
+          <div className="sign-link">
+            <Link href="sign-in" style={{textDecoration:"none"}}>Sign-in</Link>
+          </div>
+          <div className="sign-link">
+            <Link href="sign-up" style={{textDecoration:"none"}}>Sign-up</Link>
+          </div>
+        </div>
       </div>
     </main>
   )
