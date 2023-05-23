c34266e69fcb090c1685b6906d555e049b031da6
tjch-o <tzejie.c@gmail.com>
2023-05-23 19:40:54 +0800

    Add profile page feature

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main>
       <div>
         <h2>Welcome to GrindTogether!</h2>
-        <p>Check out <a href="sign-in">/sign-in</a> to see the about page</p>
+        <p>Check out <a href="study-listings">/sign-in</a> to see the about page</p>
       </div>
     </main>
   )
