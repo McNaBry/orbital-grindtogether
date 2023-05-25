064cd992a0719176ec5d6b18b5eec153f1178afc
McNaBry <mcnabry123@gmail.com>
2023-05-25 15:53:50 +0800

    Added firebase connection to the backend server
    Uses a private key that MUST be replaced during actual deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express")
+const cors = require("cors")
 
-const app = express();
+const app = express()
+const db = require("./firebase")
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended : true})) // To parse form data
@@ -11,6 +13,16 @@ app.post("/auth", (req, res) => {
   console.log(formData)
   res.redirect("http://localhost:3000/study-listings")
 });
+
+app.get("/firebase", async (req, res) => {
+  const snapshot = await db.collection('users').get()
+  const data = {}
+  snapshot.forEach((doc) => {
+    data[doc.id] = doc.data()
+  })
+  console.log("Data prepared and ready to be delivered")
+  res.send(JSON.stringify(data))
+})
 
 const port = 5000
 
