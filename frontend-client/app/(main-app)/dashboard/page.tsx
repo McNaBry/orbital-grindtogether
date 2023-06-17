c078fa80ceac6d7b111568071c12c54a9aab8ab7
McNaBry <mcnabry123@gmail.com>
2023-06-17 23:32:56 +0800

    created navbar
    shifted routes to share single layout tht provides navbar

---
+++ frontend-client/app/(main-app)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import styles from "./dashboard.module.css"
+
+export default function Dashboard() {
+  return (
+    <div id={styles["dashboard-container"]}>
+      <h1 style={{textAlign:"center"}}>Dashboard</h1>
+    </div>
+  )
+}
---
+++ frontend-client/app/(main-app)/navigationBar.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import navStyles from "./navigationBar.module.css"
+
+function NavLink({ href, value } : { href: string, value: string}) {
+  return (
+    <Nav.Link style={{color: "white"}} href={href}>{value}</Nav.Link>
+  )
+}
+
+export default function NavigationBar() {
+    return (
+      <Navbar style={{backgroundColor: "indigo", marginBottom: "10px"}} bg="" expand="lg">
+        <Container id={navStyles["nav-bar-container"]}>
+          <Navbar.Brand style={{color: "white"}}>Grind Together</Navbar.Brand>
+          <Navbar.Toggle className={navStyles["navbar-toggler"]} aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav style={{color: "white"}} className="me-auto">
+              <NavLink href={"create-listing"} value={"Create Listing"} />
+              <NavLink href={"study-listings"} value={"View Listings"} />
+              <NavLink href={"profile-page"} value={"Profile Page"} />
+              <NavLink href={"about"} value={"About GT"} />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  }
