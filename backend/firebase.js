15b46a169b45f7259264047a20eaaa29b3d6c1fe
McNaBry <mcnabry123@gmail.com>
2023-06-28 23:49:46 +0800

    change encrypted string to json file

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin')
 const { getFirestore } = require('firebase-admin/firestore');
 
 const NodeRSA = require('node-rsa')
-const fs = require('fs')
+const encryptedJSON = require('./gt-serviceacc-encrypt.json')
 const key = new NodeRSA()
 // Import private key for decryption
 key.importKey(process.env.RSA_PRIVATE_KEY, process.env.DECRYPT_SCHEME)
@@ -10,7 +10,7 @@ key.importKey(process.env.RSA_PRIVATE_KEY, process.env.DECRYPT_SCHEME)
 // const encryptedString = fs.readFileSync(__dirname + `/${process.env.SERVICE_ACC_PATH}`, 'utf-8')
 const encryptedString = process.env.SERVICE_ACC_ENCRYPT
 // Decrypt the string and parse it into a JSON object
-const serviceAccount = JSON.parse(key.decrypt(encryptedString, 'utf8'))
+const serviceAccount = JSON.parse(key.decrypt(encryptedJSON.encryptedString, 'utf8'))
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
